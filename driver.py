0cbac192863a6ae37e606be7b3148d9a0d44d539
Nelson Bighetti <bighead@bachmanity.io>
2021-03-23 18:09:24 +0900

    keep looking for earlier date

--- driver.py
+++ driver.py
@@ -1,17 +1,21 @@
 import configparser
 import time
+from dateutil import parser
+from datetime import datetime, timedelta
 from selenium import webdriver
 from selenium.webdriver.common.alert import Alert
-from selenium.webdriver.remote import switch_to
 from selenium.common.exceptions import NoSuchWindowException
+
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 URL = "https://www.hikorea.go.kr/memb/MembLoginR.pt"
 USER_ID = config['user']['id']
 USER_PASSWD = config['user']['passwd']
+USER_PHONE_NUMBER = config['user']['phone'].split('-')
 
 driver = webdriver.Chrome()
+earliest_date = datetime.now() + timedelta(days=300)
 try:
     # go to login page
     driver.get(URL)
@@ -23,48 +27,50 @@ try:
     driver.find_element_by_xpath("//a[contains(@href, 'resv') and @class='btn_apply']").click()
     driver.find_element_by_class_name("btn_blue").click()
     driver.find_element_by_class_name("btn_blue_b").click()
-    # choose time
-    main_window = driver.window_handles[0]
-    driver.find_element_by_xpath("//input[@name='deskSeq']").click()
-    driver.find_element_by_xpath("//input[@name='selBusiType1_1']").click()
-    driver.find_element_by_id("resvYmdSelect").click()
-    popup_window = driver.window_handles[1]
-    driver.switch_to.window(popup_window)
-    time.sleep(2)
+   
     # find earliest available date and time
-    try:
-        while True:
-            dates_len = len(driver.find_elements_by_xpath("//table[@class='ui-datepicker-calendar']//a"))
-            for i in range(dates_len):
-                date = driver.find_elements_by_xpath("//table[@class='ui-datepicker-calendar']//a")[i]
-                print("day: ", date.text)
-                date.click()
-                times = driver.find_elements_by_xpath("//div[@class='select_time_table']//a")
-                for t in times:
-                    t.click()
-                    try:
-                        alert = Alert(driver)
-                        alert.accept()
-                    except:
-                        print(t.text + " : no alert message")
-            driver.find_element_by_xpath("//a[@class='ui-datepicker-next ui-corner-all']").click()
-    except NoSuchWindowException as e:
-        print("found available time slot")
-        driver.switch_to.window(main_window)
-    print("window title: ", driver.title)
-    driver.find_element_by_class_name("btn_blue").click()
-    driver.find_element_by_class_name("btn_blue_b").click()
-    print("succesfully made an resvervation")
+    while True:
+        main_window = driver.window_handles[0]
+        driver.find_element_by_xpath("//input[@name='deskSeq']").click()
+        driver.find_element_by_xpath("//input[@name='selBusiType1_1']").click()
+        driver.find_element_by_xpath("//select[@id='mobileTelNo1']").send_keys(USER_PHONE_NUMBER[0])
+        driver.find_element_by_xpath("//input[@id='mobileTelNo2']").send_keys(USER_PHONE_NUMBER[1])
+        driver.find_element_by_xpath("//input[@id='mobileTelNo3']").send_keys(USER_PHONE_NUMBER[2])
+        driver.find_element_by_id("resvYmdSelect").click()
+        popup_window = driver.window_handles[1]
+        driver.switch_to.window(popup_window)
+        time.sleep(2)
+        try:
+            while True:
+                dates_len = len(driver.find_elements_by_xpath("//table[@class='ui-datepicker-calendar']//a"))
+                for i in range(dates_len):
+                    date = driver.find_elements_by_xpath("//table[@class='ui-datepicker-calendar']//a")[i]
+                    date.click()
+                    times = driver.find_elements_by_xpath("//div[@class='select_time_table']//a")
+                    for t in times:
+                        t.click()
+                        try:
+                            alert = Alert(driver)
+                            alert.accept()
+                        except:
+                            pass
+                            # print(t.text + " : no alert message")
+                driver.find_element_by_xpath("//a[@class='ui-datepicker-next ui-corner-all']").click()
+        except NoSuchWindowException as e:
+            driver.switch_to.window(main_window)
+        timeslot = driver.find_element_by_id("resvYmd").get_attribute('value')
+        print("found available timeslot: ", timeslot)
+        tempdate = parser.parse(timeslot.split()[0])
+        if tempdate < earliest_date:
+            earliest_date = tempdate 
+            driver.find_element_by_class_name("btn_blue").click()
+            driver.find_element_by_class_name("btn_blue_b").click()
+            print("succesfully made an resvervation for: ", timeslot)
+            driver.back()
+
 except Exception as e:
     print("something went wrong... ", str(e))
 finally:
     # close all windows
     time.sleep(5)
     driver.quit()
-
-# apply_btn = driver.find_element_by_class_name("btn_blue")
-# apply_btn.click()
-
-
-
-
